cae3bcc22007315d3248f0e4253f593f7ca2b63b
Gustavo Holzmann <gustavobarretohv@live.com>
2025-01-17 15:01:00 -0300

    config: initial project init

---
+++ CadenceSaver/CadenceSaver/CadenceSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceSaverApp.swift
+//  CadenceSaver
+//
+//  Created by Gustavo Holzmann on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CadenceSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
